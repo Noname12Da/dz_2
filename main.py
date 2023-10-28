6715f6ad60cafdf880830d386e16ae42561236a2
Noname12Da <148610571+Noname12Da@users.noreply.github.com>
2023-10-28 11:49:13 +0300

    No

---
+++ main.py
@@ -0,0 +1,31 @@
+# class Roborter:
+#     def __init__(self, usd, name, year, job):
+#         self.name = name
+#         self.usd = usd
+#         self.year = year
+#         self.job = job
+#
+#     def printer(self):
+#             print(f"Name: {self.name}, Usd: {self.usd}, Year: {self.year}, Job: {self.job}")
+#
+#     def grow_usd(self, usd=1):
+#         self.usd += usd
+#     def grow_year(self, year=1):
+#                 self.year += year
+#
+# roborter = Roborter(usd=500, name="Jon", year=25, job="IT")
+# roborter.printer()
+#
+# roborter.grow_usd(200)
+# roborter.grow_year(20)
+# roborter.printer()
+
+
+
+
+
+
+
+
+
+
